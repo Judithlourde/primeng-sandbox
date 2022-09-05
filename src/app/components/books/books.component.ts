7314e259946db2a50428f8e53b3a99f1b1d4ddf1
judithlourde <judith.lourde@gmail.com>
2022-09-05 09:41:58 +0200

    Change books data to book service

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../services/book.service';
 import { Book } from '../../types/Book';
 
 
@@ -9,39 +10,21 @@ import { Book } from '../../types/Book';
 })
 export class BooksComponent implements OnInit {
   
+  constructor( private bookService: BookService ) { 
+    
+    // console.log({Constructor: 'Constructor'})
+  }
+  books: Book[] = [];
+
   value!: string;
   isShowing: boolean = false;
   isDisabled: boolean = false;
 
   cart: Book[] = [];
 
-  books: Book[] = [
-    {
-      name: "The Cat Kid Comic Club",
-      author: "Dav Pilkey",
-      image: "https://riachristiecollections.com/wp-content/uploads/2022/06/9781338801941-5.jpg",
-      amount: 100
-    },
-    {
-      name: "The 20th-Century Children's Book Treasury",
-      author: "Janet Schulman",
-      image: "https://d3525k1ryd2155.cloudfront.net/f/471/886/9780679886471.RH.0.m.jpg",
-      amount: 150
-    },
-    {
-      name: "I Love You to the Moon And Back",
-      author: "Little Tiger Press",
-      image: "https://images.kidsbooks.com/covers/large/isbn978158/9781589255517-l.jpg",
-      amount: 95
-    }
-  ];
-
-  constructor() { 
-    console.log({Constructor: 'Constructor'})
-  }
-
   ngOnInit(): void {
-    console.log({OnInIt: 'From OnIt'})
+    // console.log({OnInIt: 'From OnIt'})
+    this.books = this.bookService.getBooks();
   }
  
 
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -5,5 +5,27 @@ import { Injectable } from '@angular/core';
 })
 export class BookService {
 
-  constructor() { }
+  constructor() {}
+  getBooks() {
+    return [
+      {
+        name: "The Cat Kid Comic Club",
+        author: "Dav Pilkey",
+        image: "https://riachristiecollections.com/wp-content/uploads/2022/06/9781338801941-5.jpg",
+        amount: 100
+      },
+      {
+        name: "The 20th-Century Children's Book Treasury",
+        author: "Janet Schulman",
+        image: "https://d3525k1ryd2155.cloudfront.net/f/471/886/9780679886471.RH.0.m.jpg",
+        amount: 150
+      },
+      {
+        name: "I Love You to the Moon And Back",
+        author: "Little Tiger Press",
+        image: "https://images.kidsbooks.com/covers/large/isbn978158/9781589255517-l.jpg",
+        amount: 95
+      }
+    ];
+  } 
 }

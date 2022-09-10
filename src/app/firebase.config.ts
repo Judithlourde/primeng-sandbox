a0c7ab9e0a52022723912153b6d6d431209d9d33
judithlourde <judith.lourde@gmail.com>
2022-09-10 16:13:10 +0200

    Set deploying settings

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCaRzI4Jfj7YBv7AP7uxaQDCpmqJcx3wnk",
+  authDomain: "book-shop-angular.firebaseapp.com",
+  projectId: "book-shop-angular",
+  storageBucket: "book-shop-angular.appspot.com",
+  messagingSenderId: "376106264676",
+  appId: "1:376106264676:web:bdebbdbe8b94319dcad771"
+};
+
+// Initialize Firebase
+// export const firebaseApp = initializeApp(firebaseConfig);
